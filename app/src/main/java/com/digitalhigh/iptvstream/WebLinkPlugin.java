49ab3928b48baa22a5783c9f31b450038e2dd980
d8ahazard <d8ahazard@gmail.com>
2017-08-31 10:28:43 -0500

    Hey, it actually works!
    Neato

--- app/src/main/java/com/digitalhigh/iptvstream/WebLinkPlugin.java
+++ app/src/main/java/com/digitalhigh/iptvstream/WebLinkPlugin.java
@@ -3,6 +3,12 @@ package com.digitalhigh.iptvstream;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.webkit.URLUtil;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.felkertech.cumulustv.plugins.CumulusChannel;
 import com.felkertech.cumulustv.plugins.CumulusTvPlugin;
@@ -23,46 +29,71 @@ import java.util.concurrent.ExecutionException;
 public class WebLinkPlugin extends CumulusTvPlugin {
     private static final String TAG = WebLinkPlugin.class.getSimpleName();
     private String JSONinput;
+    private ProgressBar pb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_layout);
         setLabel("");
         setProprietaryEditing(false);
+        pb = (ProgressBar) findViewById(R.id.progressBar2);
+        pb.setVisibility(View.INVISIBLE);
+        Button inputButton = (Button) findViewById(R.id.button2);
+        inputButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG,"It's the button, dude.");
+                EditText et = (EditText) findViewById(R.id.editText);
+                String inUrl = et.getText().toString().trim();
+                Log.d(TAG,"URL "+ inUrl);
+                if (URLUtil.isValidUrl(inUrl)) {
+                    pb.setVisibility(View.VISIBLE);
+                    importJson(inUrl);
+                } else {
+                    Toast.makeText(getApplicationContext(),"Invalid URL provided.",Toast.LENGTH_LONG);
+                }
+            }
+        });
+
     }
 
-    public void importJson() {
+    public void importJson(String fetchUrl) {
         Log.d(TAG, "ImportJSON called.");
         try {
-            JSONinput = new JsonTask().execute("http://FOO/IPTV/app.php?JSON=true").get();
+            JSONinput = new JsonTask().execute(fetchUrl).get();
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
         JSONObject json;
+
         try {
             json = new JSONObject(JSONinput);
             JSONArray channels = json.getJSONArray("channels");
             for (int i = 0; i < channels.length(); i++) {
                 JSONObject value = channels.getJSONObject(i);
                 Log.d(TAG, "Channel: " + value.getString("name"));
-                CumulusChannel c = new CumulusChannel.Builder()
+
+                CumulusChannel.Builder b = new CumulusChannel.Builder()
                         .setName(value.getString("name"))
                         .setNumber(value.getString("number"))
                         .setMediaUrl(value.getString("url"))
-                        .setEpgUrl(value.getString("epgUrl"))
                         .setLogo(value.getString("logo"))
                         .setSplashscreen(value.getString("splashscreen"))
-                        .setGenres(value.getString("genres"))
-                        .build();
+                        .setGenres(value.getString("genres"));
+                if (value.has("epgUrl")) b.setEpgUrl(value.getString("epgUrl"));
+                CumulusChannel c = b.build();
                 finish();
                 saveChannel(c);
             }
-        } catch (JSONException e) {
+        } catch (NullPointerException | JSONException e) {
             e.printStackTrace();
         }
     }
+
+
+
 
     private class JsonTask extends AsyncTask<String, String, String> {
 
@@ -114,7 +145,9 @@ public class WebLinkPlugin extends CumulusTvPlugin {
 
         @Override
         protected void onPostExecute(String JSONout) {
+
             super.onPostExecute(JSONout);
+            pb.setVisibility(View.INVISIBLE);
         }
     }
 }
